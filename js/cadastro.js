2f04bc76567b7dd11ae2438db8a48baeb13ac6ba
Marcus Paulo <marcuspaulo.morenomf@gmail.com>
2022-10-28 00:46:49 -0300

    ajeitando js

--- js/cadastro.js
+++ js/cadastro.js
@@ -5,30 +5,33 @@ botao.addEventListener('click', (e)=>{
 
     const select = document.querySelector('#usuario')
     let funcao = select.options[select.selectedIndex].text
-    console.log(funcao)
-
+    
     const nome = document.querySelector('#nome').value
-    console.log(nome)
-
+    
     const nascimento = document.querySelector('#nasc').value
-    console.log(nascimento)
-
+    
     const email = document.querySelector('#email').value
-    console.log(email)
-
+    
     const senha = document.querySelector('#senha').value
-    console.log(senha)
-
-
+    
+    
     try{
         if(funcao == 'Qual sua função?') throw "Selecionar uma função!"
         if(nome == '') throw "Campo de Nome completo OBRIGATÓRIO!"
         if(nascimento == '') throw "Campo de Nascimento OBRIGATÓRIO!"
         if(email == '') throw "Campo de Email OBRIGATÓRIO!"
         if(senha == '') throw "Campo de Senha OBRIGATÓRIO!"
-
-
+        
+        
     } catch(err){
         alert(err)
+        return
     }
+    
+    
+    console.log(funcao)
+    console.log(nome)
+    console.log(nascimento)
+    console.log(email)
+    console.log(senha)
 })
