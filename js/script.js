dfa234217dd973f0de2092b172a9c1bed1ecf359
Gabriel Albuquerque <gabriel.albuquerque@dbccompany.com.br>
2022-10-28 10:09:46 -0300

    Home - V5 - Verify Log

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
+const failMsgLogin = 'Insira e-mail e senha para efetuar o login!'
+
 function login() {
     const loginValue = document.getElementById('home-email').value;
     const passValue = document.getElementById('home-pass').value;
+
+    if (loginValue == false) {
+        alert(failMsgLogin)
+        location.reload();
+    } else if (passValue == false) {
+        alert(failMsgLogin)
+        location.reload();
+    }
+    
     console.log(loginValue);
     console.log(passValue);
 }
