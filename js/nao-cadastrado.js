fea03d19ba3defb30e1aebe506069c59f81423ef
Marcus Paulo <marcuspaulo.morenomf@gmail.com>
2022-10-29 12:45:53 -0300

    finalizado tela cadastro

--- js/nao-cadastrado.js
+++ js/nao-cadastrado.js
@@ -8,8 +8,11 @@ const remuneracao = document.querySelector('#remuneracao')
 const tituloVaga = document.querySelector('#titulo-vaga')
 const descricaoVaga = document.querySelector('#descricao-vaga')
 
+const naoCandidatado = document.querySelector('#nao-cadidatado')
+const button = document.querySelector('#button')
 
-let array =[{nome: 'Marcus', nascimento: '10/12/1999'},{nome:'Roberta', nascimento: '20/02/2001'}]
+
+let array =[]
 
 
 function mostraCandidato(){
@@ -21,16 +24,19 @@ function mostraCandidato(){
 
 const parametro = new URLSearchParams(window.location.search)
 
+function pegarVaga(){
+    axios
+    .get(`${url}/vagas/${parametro.get('id')}`)
+    .then((response) => {
+      vaga = response.data;
+      console.log(vaga);
+      array = response.data.candidatos
+      candidatar()  
+      mostraCandidato()
+      cadidatado()
+    })   
+}
 
-axios
-.get(`${url}/vagas/${parametro.get('id')}`)
-.then((response) => {
-  vaga = response.data;
-  console.log(vaga);
-  array = response.data.candidatos
-  candidatar()  
-  mostraCandidato()
-})
 
 function candidatar() {
     idVaga.innerText = `ID da vaga: ${parametro.get('id')}`
@@ -42,13 +48,16 @@ function candidatar() {
     descricaoVaga.innerHTML = `<span>Descrição da vaga: </span>${vaga.description}`
 }
 
-
-axios.get(`${url}/users/${localStorage.getItem('@vemserjs-userId')}`,prepareHeaders())
-    .then(response=>{
-      userInfo = response.data
-      console.log(userInfo)
-      delete userInfo.password
-    })
+function pegarUsuario(){
+    axios.get(`${url}/users/${localStorage.getItem('@vemserjs-userId')}`,prepareHeaders())
+        .then(response=>{
+          userInfo = response.data
+          console.log(userInfo)
+          delete userInfo.password
+          pegarVaga()
+        })
+}
+pegarUsuario()
 
 function prepareHeaders(){
     let token = localStorage.getItem('@vemserjs-token')
@@ -60,7 +69,40 @@ function prepareHeaders(){
 }    
 
 function candidatarVaga() {
-    axios.patch(`${url}/vagas/${parametro.get('id')}`, {candidatos: [...array,{nome:userInfo.nome, id: userInfo.id, nascimento: userInfo.nascimento}]}, prepareHeaders()).then(response =>{
+    axios.patch(`${url}/vagas/${parametro.get('id')}`, {candidatos: [...array,{nome:userInfo.nome, id: userInfo.id, nascimento: userInfo.nascimento, reprovado: false}]}, prepareHeaders()).then(response =>{
         console.log(response.data)
+        cadidatado()
     })
+
+}
+
+function cadidatado(){
+    let item = array.find(item => item.id == localStorage.getItem('@vemserjs-userId'))
+    if(item){
+        if(item.reprovado){
+            button.removeAttribute('onclick')
+            button.classList.add('botao-cinza')
+            button.classList.remove('botao-azul')
+            button.innerText = 'Cancelar candidatura'
+        } else {
+            button.removeAttribute('onclick')
+            button.classList.add('botao-vermelho')
+            button.classList.remove('botao-azul')
+            button.innerText = 'Cancelar candidatura'
+            button.addEventListener('click', ()=> cancelarCandidatura())
+                
+        }
+    } 
+}
+
+function cancelarCandidatura() {
+    console.log('oi')
+    let item = array.findIndex(item => item.id == localStorage.getItem('@vemserjs-userId'))
+    if(item > -1){
+        array.splice(item)
+        axios.patch(`${url}/vagas/${parametro.get('id')}`, {candidatos: [...array]}, prepareHeaders()).then(response =>{
+            console.log(response.data)
+            pegarVaga()
+        })
+    }
 }
